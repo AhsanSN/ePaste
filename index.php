d68b7979743f230198069419cbfef206209aea81
Syed Ahsan Ahmed <snahmed1998@gmail.com>
2018-08-15 11:59:14 +0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+echo"this works";
+
+
+
+// current directory
+echo getcwd() . "\n";
+
+chdir('C:\Users\Ahsan Ahmed\Documents\GitHub\FamiChat-Android\www');
+
+// current directory
+echo getcwd() . "\n";
+
+include("index.html");
+
+?>
